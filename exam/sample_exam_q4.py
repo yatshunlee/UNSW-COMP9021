c9be41645892e34063707c357b5513773d2b32f3
Jasper Lee <69416199+yatshunlee@users.noreply.github.com>
2024-04-29 20:32:14 -0400

    Add files via upload

---
+++ exam/sample_exam_q4.py
@@ -0,0 +1,65 @@
+# Sample Exam Question 4
+
+
+'''
+Will be tested with a at least equal to 2 and b at most equal to 10_000_000.
+'''
+
+import sys
+from math import sqrt
+
+
+def f(a, b):
+    '''
+    >>> f(2, 2)
+    There is a unique prime number beween 2 and 2.
+    >>> f(2, 3)
+    There are 2 prime numbers between 2 and 3.
+    >>> f(2, 5)
+    There are 3 prime numbers between 2 and 5.
+    >>> f(4, 4)
+    There is no prime number beween 4 and 4.
+    >>> f(14, 16)
+    There is no prime number beween 14 and 16.
+    >>> f(3, 20)
+    There are 7 prime numbers between 3 and 20.
+    >>> f(100, 800)
+    There are 114 prime numbers between 100 and 800.
+    >>> f(123, 456789)
+    There are 38194 prime numbers between 123 and 456789.
+    '''
+
+    number_of_primes_at_most_equal_to_b = 0
+
+    from collections import defaultdict
+    lookup = defaultdict(int)
+
+    # Insert your code here
+    for i in range(2, b+1):
+        # if is not prime
+        if lookup[i] == 1:
+            continue
+
+        k = i + i
+        while k <= b:
+            lookup[k] = 1
+            k = k + i
+
+    for i in range(a, b + 1):
+        if lookup[i]:
+            continue
+        number_of_primes_at_most_equal_to_b += 1
+
+
+    if not number_of_primes_at_most_equal_to_b:
+        print(f'There is no prime number beween {a} and {b}.')
+    elif number_of_primes_at_most_equal_to_b == 1:
+        print(f'There is a unique prime number beween {a} and {b}.')
+    else:
+        print(f'There are {number_of_primes_at_most_equal_to_b} prime numbers between {a} and {b}.')
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()
---
+++ exam/sample_exam_q8.py
@@ -0,0 +1,124 @@
+# Sample Exam Question 8
+
+# Write a function that accepts a string of DISTINCT UPPERCASE letters only
+# called letters and displays all pairs of words using all (distinct) letters in letters.
+
+# Please note that the words need to be valid. Use the provided dictionary.txt
+# to check the validity of words.
+
+'''
+Will be tested with letters, a string of DISTINCT UPPERCASE letters only.
+'''
+
+
+
+def f(letters):
+    '''
+    >>> f('ABCDEFGH')
+    There is no solution
+    >>> f('GRIHWSNYP')
+    The pairs of words using all (distinct) letters in "GRIHWSNYP" are:
+    ('SPRING', 'WHY')
+    >>> f('ONESIX')
+    The pairs of words using all (distinct) letters in "ONESIX" are:
+    ('ION', 'SEX')
+    ('ONE', 'SIX')
+    >>> f('UTAROFSMN')
+    The pairs of words using all (distinct) letters in "UTAROFSMN" are:
+    ('AFT', 'MOURNS')
+    ('ANT', 'FORUMS')
+    ('ANTS', 'FORUM')
+    ('ARM', 'FOUNTS')
+    ('ARMS', 'FOUNT')
+    ('AUNT', 'FORMS')
+    ('AUNTS', 'FORM')
+    ('AUNTS', 'FROM')
+    ('FAN', 'TUMORS')
+    ('FANS', 'TUMOR')
+    ('FAR', 'MOUNTS')
+    ('FARM', 'SNOUT')
+    ('FARMS', 'UNTO')
+    ('FAST', 'MOURN')
+    ('FAT', 'MOURNS')
+    ('FATS', 'MOURN')
+    ('FAUN', 'STORM')
+    ('FAUN', 'STROM')
+    ('FAUST', 'MORN')
+    ('FAUST', 'NORM')
+    ('FOAM', 'TURNS')
+    ('FOAMS', 'RUNT')
+    ('FOAMS', 'TURN')
+    ('FORMAT', 'SUN')
+    ('FORUM', 'STAN')
+    ('FORUMS', 'NAT')
+    ('FORUMS', 'TAN')
+    ('FOUNT', 'MARS')
+    ('FOUNT', 'RAMS')
+    ('FOUNTS', 'RAM')
+    ('FUR', 'MATSON')
+    ('MASON', 'TURF')
+    ('MOANS', 'TURF')
+    '''
+    dictionary = 'dictionary.txt'
+    solutions = []
+
+    # Insert your code here
+    with open(dictionary, 'r') as infile:
+        words = sorted(infile.read().split('\n'))
+
+    # filter irrelevant (key step)
+    l_set = set(letters)
+    words = [w for w in words if len(set(w).difference(l_set))==0]
+
+    def is_valid_pair(word1, word2, letters):
+        word_set = word1 + word2
+        return len(word_set) == len(letters) and set(word_set) == set(letters)
+
+    def find_pairs(letters, dictionary):
+        valid_pairs = []
+        for i in range(len(dictionary)):
+            word1 = dictionary[i]
+            for j in range(i + 1, len(dictionary)):
+                word2 = dictionary[j]
+                if is_valid_pair(word1, word2, letters):
+                    pair = tuple(sorted([word1, word2]))
+                    if pair in valid_pairs:
+                        continue
+                    valid_pairs.append(pair)
+        return valid_pairs
+
+    solutions = find_pairs(letters, words)
+
+    # d = {}
+    # for i in range(len(words)):
+    #     wd1 = words[i]
+    #     for j in range(i+1, len(words)):
+    #         wd2 = words[j]
+    #         wd_set = wd1 + wd2
+    #
+    #         n = len(wd_set)
+    #         s = set(wd_set)
+    #         s = tuple(sorted(list(s)))
+    #         d.setdefault(n, {})
+    #         d[n].setdefault(s, [])
+    #         d[n][s].append((wd1, wd2))
+    #
+    # ns = len(letters)
+    # ss = set(letters)
+    # ss = tuple(sorted(list(ss)))
+
+    # solutions = d.get(ns).get(ss)
+
+    if not solutions:
+        print('There is no solution')
+    else:
+        print(f'The pairs of words using all (distinct) letters in "{letters}" are:')
+        for solution in solutions:
+            print(solution)
+
+
+if __name__ == '__main__':
+
+    import doctest
+
+    doctest.testmod()
